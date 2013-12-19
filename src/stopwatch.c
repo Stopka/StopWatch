b5be07eec40b8108f5f209ef52ea29d542c3a4fc
Štěpán Škorpil <stepan.s@gmail.com>
2013-12-19 15:44:38 -0800

    Lap graphics
    Added time unit labels to the time display
    Added basic laps display
    Added action bar icons

--- src/stopwatch.c
+++ src/stopwatch.c
@@ -2,14 +2,19 @@
 	
 #define FONT_TIME_DISPLAY RESOURCE_ID_FONT_DEJAVU_SANS_BOLD_25
 #define FONT_TIME_DISPLAY_LABEL RESOURCE_ID_FONT_DEJAVU_SANS_12
+#define FONT_LAPS_DISPLAY_LAP RESOURCE_ID_FONT_DEJAVU_SANS_19
+	
+#define ICON_RESET RESOURCE_ID_ICON_RESET
+#define ICON_LAP RESOURCE_ID_ICON_LAP
+#define ICON_START RESOURCE_ID_ICON_STOPWATCH_START
+#define ICON_STOP RESOURCE_ID_ICON_STOPWATCH_STOP
+#define ICON_NEXT RESOURCE_ID_ICON_DOWN
 //window
 static Window* stopwatch_window;
 //Time display
 static Layer *time_display;
 static TextLayer* time_display_values;
-static TextLayer* time_display_labels[3];
-static InverterLayer* time_display_line;
-static int measure_offset=2;
+static TextLayer* time_display_labels[7];
 char *measure_labels[5] = {"d","h","m","s","ms"};
 //Laps
 static ScrollLayer* laps_display;
@@ -17,57 +22,79 @@ static TextLayer* laps_display_laps[30];
 //Action bar
 static ActionBarLayer* action_bar;	
 
-
+//STATE
+static int measure_offset=2;
+static int measure_offset_laps=2;
+static bool running=false;
 
 static void window_load(Window *window) {
 	///////////////////////////////////////////////////////////////////
 	// Time display
 	///////////////////////////////////////////////////////////////////
-	time_display=layer_create(GRect(0, 0, 144, 40));
+	time_display=layer_create(GRect(0, 0, 144, 46));
 	layer_add_child(window_get_root_layer(window), (Layer*)time_display);
 	
 	// Values ////////////////////////////////////////////////////////////
-	time_display_values=text_layer_create(GRect(0, -3, 122, 40));
-	/*time_display_values[1]=text_layer_create(GRect(50, 0, 50, 40));
-	time_display_values[2]=text_layer_create(GRect(44, 0, 20, 30));*/
+	time_display_values=text_layer_create(GRect(0, 6, 122, 40));
 	GFont font = fonts_load_custom_font(resource_get_handle(FONT_TIME_DISPLAY));
-	//for(int i=0;i<3;i++){
-		text_layer_set_font(time_display_values,font);
-		text_layer_set_background_color	(time_display_values,GColorClear);
-		text_layer_set_text_color(time_display_values,GColorWhite);
-		text_layer_set_text_alignment(time_display_values, GTextAlignmentCenter);
-		text_layer_set_overflow_mode(time_display_values,GTextOverflowModeFill); 
-		text_layer_set_text	(time_display_values,"00:00.00");
-		layer_add_child((Layer *)time_display, (Layer *)time_display_values);
-	//}
+	text_layer_set_font(time_display_values,font);
+	text_layer_set_background_color	(time_display_values,GColorClear);
+	text_layer_set_text_color(time_display_values,GColorWhite);
+	text_layer_set_text_alignment(time_display_values, GTextAlignmentCenter);
+	text_layer_set_overflow_mode(time_display_values,GTextOverflowModeFill); 
+	text_layer_set_text	(time_display_values,"00:00.00");
+	layer_add_child((Layer *)time_display, (Layer *)time_display_values);
 	
 	// Labels ////////////////////////////////////////////////////////////
-	time_display_labels[0]=text_layer_create(GRect(0, 27, 40, 20));
-	time_display_labels[1]=text_layer_create(GRect(40, 27, 40, 20));
-	time_display_labels[2]=text_layer_create(GRect(80, 27, 40, 20));
+	time_display_labels[0]=text_layer_create(GRect(0, 0, 40, 14));
+	time_display_labels[1]=text_layer_create(GRect(40, 0, 41, 14));
+	time_display_labels[2]=text_layer_create(GRect(81, 0, 41, 14));
+	
+	time_display_labels[6]=text_layer_create(GRect(0, 32, 32, 14));
+	time_display_labels[3]=text_layer_create(GRect(32, 32, 30, 14));
+	time_display_labels[4]=text_layer_create(GRect(62, 32, 30, 14));
+	time_display_labels[5]=text_layer_create(GRect(92, 32, 30, 14));
 	font = fonts_load_custom_font(resource_get_handle(FONT_TIME_DISPLAY_LABEL));
-	for(int i=0;i<3;i++){
+	for(int i=0;i<6;i++){
 		text_layer_set_font(time_display_labels[i],font);
-		text_layer_set_background_color	(time_display_labels[i],GColorClear);
-		text_layer_set_text_color(time_display_labels[i],GColorWhite);
+		text_layer_set_background_color	(time_display_labels[i],(i<3?GColorClear:GColorWhite));
+		text_layer_set_text_color(time_display_labels[i],(i<3?GColorWhite:GColorBlack));
 		text_layer_set_text_alignment(time_display_labels[i], GTextAlignmentCenter);
-		text_layer_set_overflow_mode(time_display_labels[i],GTextOverflowModeFill); 
-		text_layer_set_text	(time_display_labels[i],measure_labels[measure_offset+i]);
+		text_layer_set_overflow_mode(time_display_labels[i],GTextOverflowModeFill);
+		text_layer_set_text	(time_display_labels[i],measure_labels[(i<3?measure_offset:measure_offset_laps)+(i%3)]);
 		layer_add_child((Layer *)time_display, (Layer *)time_display_labels[i]);
 	}
-	
-	// Line ////////////////////////////////////////////////////////////
-	time_display_line=inverter_layer_create(GRect(0, 28, 121, 22));
-	layer_add_child((Layer *)time_display, (Layer *)time_display_line);
+	text_layer_set_font(time_display_labels[6],font);
+	text_layer_set_background_color	(time_display_labels[6],GColorWhite);
+	text_layer_set_text_color(time_display_labels[6],GColorBlack);
+	text_layer_set_text_alignment(time_display_labels[6], GTextAlignmentCenter);
+	text_layer_set_overflow_mode(time_display_labels[6],GTextOverflowModeFill);
+	text_layer_set_text	(time_display_labels[6],"#");
+	layer_add_child((Layer *)time_display, (Layer *)time_display_labels[6]);
 	
 	///////////////////////////////////////////////////////////////////
 	// Laps display
 	///////////////////////////////////////////////////////////////////
-	laps_display=scroll_layer_create(GRect(0,50,121,146));
-	layer_add_child(window_get_root_layer(window), (Layer*)time_display);
+	laps_display=scroll_layer_create(GRect(0,46,121,146));
+	layer_add_child(window_get_root_layer(window), (Layer*)laps_display);
 	
 	// Laps ////////////////////////////////////////////////////////////
+	laps_display_laps[0]=text_layer_create(GRect(0, 0, 121, 20));
+	font=fonts_load_custom_font(resource_get_handle(FONT_LAPS_DISPLAY_LAP));
+	text_layer_set_font(laps_display_laps[0],font);
+	text_layer_set_background_color	(laps_display_laps[0],GColorClear);
+	text_layer_set_text_color(laps_display_laps[0],GColorWhite);
+	text_layer_set_text_alignment(laps_display_laps[0], GTextAlignmentCenter);
+	text_layer_set_text	(laps_display_laps[0],"01 00:00.00");
+	scroll_layer_add_child(laps_display, (Layer *)laps_display_laps[0]);
 	
+	laps_display_laps[1]=text_layer_create(GRect(0, 20, 121, 20));
+	text_layer_set_font(laps_display_laps[1],font);
+	text_layer_set_background_color	(laps_display_laps[1],GColorClear);
+	text_layer_set_text_color(laps_display_laps[1],GColorWhite);
+	text_layer_set_text_alignment(laps_display_laps[1], GTextAlignmentCenter);
+	text_layer_set_text	(laps_display_laps[1],"02 00:00.00");
+	scroll_layer_add_child(laps_display, (Layer *)laps_display_laps[1]);
 	
 	///////////////////////////////////////////////////////////////////
 	// Action bar
@@ -75,6 +102,9 @@ static void window_load(Window *window) {
 	action_bar = action_bar_layer_create();
 	action_bar_layer_add_to_window(action_bar, window);
 	action_bar_layer_set_background_color(action_bar, GColorWhite);
+	action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, gbitmap_create_with_resource(ICON_RESET));
+	action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, gbitmap_create_with_resource(ICON_START));
+  	action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, gbitmap_create_with_resource(ICON_NEXT));
 }
 
 static void window_appear(Window *window) {
@@ -86,12 +116,18 @@ static void window_disappear(Window *window) {
 }
 
 static void window_unload(Window *window) {
+	//Time display
 	layer_destroy(time_display);
 	text_layer_destroy	(time_display_values);
-	for(int i=0;i<3;i++){
+	for(int i=0;i<7;i++){
 		text_layer_destroy	(time_display_labels[i]);		
 	}
-	inverter_layer_destroy(time_display_line);
+	//Laps display
+	scroll_layer_destroy(laps_display);
+	for(int i=0;i<2;i++){
+		text_layer_destroy(laps_display_laps[i]);
+	}
+	//Action bar
 	action_bar_layer_destroy(action_bar);
 }
 
