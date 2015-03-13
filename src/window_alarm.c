99e83afe0bdb49045f3e7d00ae88a868975a35dd
Štěpán Škorpil <stepan.s@gmail.com>
2015-03-13 12:45:02 +0100

    Created alarm window skelet

---
+++ src/window_alarm.c
@@ -0,0 +1,55 @@
+#include <pebble.h>
+#include "window_alarm.h"
+
+static void window_load(Window* window);
+static void window_appear(Window* window);
+static void window_disappear(Window* window);
+static void window_unload(Window* window);
+	
+static Window* window;
+
+void window_alarm_show() {
+	window_stack_push(window, true);
+}
+
+void window_alarm_init(){
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_init()");
+	window= window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+		.load = window_load,
+		.appear = window_appear,
+		.disappear = window_disappear,
+		.unload = window_unload
+	});
+}
+
+void window_alarm_deinit(){
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_deinit()");
+	window_destroy(window);
+}
+
+
+
+static void window_load(Window* window){
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_load()");
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_frame(window_layer);
+	
+	
+	window_set_status_bar_icon(window,bitmaps_get_bitmap(RESOURCE_ID_TIMER16));	
+}
+
+static void window_appear(Window *window) {
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_appear()");
+	
+}
+
+static void window_disappear(Window *window) {
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_disappear()");
+}
+
+
+static void window_unload(Window* window){
+	APP_LOG(APP_LOG_LEVEL_INFO,"window_unload()");
+	
+}
---
+++ src/window_alarm.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void window_alarm_init();
+void window_alarm_deinit();
+void window_alarm_show();
